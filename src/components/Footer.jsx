93dd5929437b19b426398a3cdeeb42f1a372239c
jyotsnayall <jyotsnasharma2603@gmail.com>
2022-08-01 23:21:16 +0530

    created react project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      Copyright © 2010-2022   <span className="font-bold">Khushi Softech</span> All Rights Reserved.
+      {/* © Copyright 2010 - 2022. Khushi Softech Powered By KST */}
+    </p>
+  </div>
+);
+
+export default Footer;
---
+++ temp/him.js
@@ -0,0 +1,2 @@
+'hero-pattern':
+            "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')",
